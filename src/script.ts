1bcd2342c6cd886bcf53f50e7246c9936f1f39c7
David Andersson <lejonmanen@gmail.com>
2024-08-19 14:17:41 +0200

    Exempel

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,28 @@
+// 3 Skapa en variabel color som innehåller namnet på ett färg.Skriv en funktion som producerar en mening med variabeln.Om color till exempel är 'blå' ska programmet skriva ut: 'En blå bil'.Variabeln och funktionen ska ha type annotations; dvs du ska tala om vilka datatyper variabeln, parametern och returvärdet har.
 
-let x: number = 55
+let color: string
+color = 'blå'
+console.log( carSentence(color) )
 
-x++
+function carSentence(carColor: string): string {
+	// let sentence = 'En '
+	// sentence += carColor
+	// sentence += 'bil'
+	// let sentence: string = 'En ' + carColor + 'bil'
+	let sentence: string = `En ${carColor} bil`
+	return sentence
+	// console.log(`En ${carColor} bil`)
+}
 
-// x += 'hej'
 
-console.log(x)  // '56hej'
+let xs: number[] = [1, 2, 5, 10]
+let madness: any = 1
+madness = 'hejsan'
+madness = [1, 'hej', true]
+
+interface Box {
+	value: number;
+}
+let box: Box = { value: 55 }
+console.log( box.value )
+// box.name - funkar ej
